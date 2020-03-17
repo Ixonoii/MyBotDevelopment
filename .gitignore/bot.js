31c557788ec95e011bfcfff5b10ebe1466344a85
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-03-17 18:59:10 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -8,6 +8,15 @@ client.login(process.env.BotToken)
 
 // BOT ADDED & REMOVED
 
-client.on("guildCreate", guild => {
-    client.channels.get("689513780870381568").send("MyBot has been added on a server! (Testing)")
+client.on("guildCreate", guild =>{
+    let BotAddedEmbed = new Discord.RichEmbed()
+    .setColor("0xf35353")
+    .setTitle("MyBot has been added on a server.")
+    .setDescription(`MyBot is now on ${client.guilds.size} servers.`)
+    .addField("**Server**", guild.name + "(``" + guild.id + "``)", true)
+    .addField("**Owner**", guild.owner + "(``" + guild.ownerID + "``)", true)
+    .addField("**Members**", guild.memberCount, true)
+    .setThumbnail(guild.iconURL)
+    .setTimestamp()
+    client.channels.get("689513780870381568").send(BotAddedEmbed)
 })
