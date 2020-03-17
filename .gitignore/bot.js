ee18f673c5f859a0810f409fcd51b63e19b567cd
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-03-17 18:48:29 +0100

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -8,24 +8,6 @@ client.login(process.env.BotToken)
 
 // BOT ADDED & REMOVED
 
-client.on("guildCreate", guild =>{
-    var BotAddedEmbed = new Discord.RichEmbed()
-    .setColor("#00ff3c")
-    .setTitle("MyBot has been added on a server.")
-    .setDescription(`I am now on ${client.guilds.size} servers !`)
-    .setImage(guild.iconURL)
-    .addField("Information about the server:", "Name: **" + guild.name + "** (``" + guild.id + "``) \n Members: **" + guild.memberCount + "** \n Owner: **" + guild.owner + "** (``" + guild.ownerID + "``)")
-    .setTimestamp()
-    client.channels.get("689513780870381568").send(BotAddedEmbed)
-})
-
-client.on("guildDelete", guild =>{
-    var BotAddedEmbed = new Discord.RichEmbed()
-    .setColor("#ff0000")
-    .setTitle("MyBot has been removed from a server.")
-    .setDescription(`I am now on ${client.guilds.size} servers !`)
-    .setImage(guild.iconURL)
-    .addField("Information about the server:", "Name: **" + guild.name + "** (``" + guild.id + "``) \n Members: **" + guild.memberCount + "** \n Owner: **" + guild.owner + "** (``" + guild.ownerID + "``)")
-    .setTimestamp()
-    client.channels.get("689513780870381568").send(BotAddedEmbed)
+client.on("guildCreate", guild => {
+    client.channels.get("689513780870381568").send("MyBot has been added on a server! (Testing)")
 })
