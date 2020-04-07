934202a14b0320d59f7403c1d543ebe96e26a866
Ixonoii <54681124+Ixonoii@users.noreply.github.com>
2020-04-07 16:07:48 +0200

    Update bot.js

--- .gitignore/bot.js
+++ .gitignore/bot.js
@@ -1,42 +1,60 @@
-const fs = require("fs")
-const roblox = require("noblox.js")
-const discord = require("discord.js") 
+const roblox = require('noblox.js')
+const Discord = require('discord.js')
+const client = new Discord.Client();
+ 
+var cookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlMGViNzMzYS0wYmY5LTRkNjEtOTRhYS02MzJmNDYzYjVhNjYiLCJzdWIiOjMyNDExMDg5M30.h5VYephbcEXyl3CNtjevtt9c1J0ZdI7DKndNwuPzvLY";
+var prefix = '#';
+var groupId = 5063409;
+var maximumRank = 10;
+ 
+function login() {
+    return roblox.cookieLogin(cookie);
+}
 
-let month = dateObj.getMonth()
-let day = dateObj.getDate()
-let year = dateObj.getFullYear()
-
-client.on("message", message => {
-    if(!message.guild) return
-    let args = message.content.trim().split(/ +/g)
-
-    if(args[0].toLowerCase() === '-info') {
-        let username = args[0]
-        if (username) {
-            roblox.getIdFromUsername(username).then(id =>{
-                if (id) {
-                    let embed = new discord.RichEmbed() // starts a new embed
-
-                   .setColor("#f9ae00") // sets the color of the embed
-                    .setURL(`https://roblox.com/users/${id}/profile`) // base link, changed by the variables 'id'
-                    .setTimestamp()
-                    .setThumbnail(`https://www.roblox.com/bust-thumbnail/image?userId=${id}&width=420&height=420&format=png`) // gets the roblox profile picture
-
-                    .addField("Username", info.username || 'Unresolvable', true) // everything in the embed is undefined, therefore can be changed by the variables
-                    .addField("User ID", id || 'Unresolvable', true)
-                    .addField("Blurb", info.blurb || 'Nothing', true)
-                    .addField("Status", info.status || 'Nothing', true)
-                    .addField("Account Age", `${info.age} days old` || 'Unresolvable')
-                    .addField("Register Date", `${dateInfo.month}/${dateInfo.day}/${dateInfo.year}` || 'Unresolvable')
-                    .addField("User Link", `https://roblox.com/users/${id}/profile`)
-                    .setFooter(`Search Bot`, bot.user.avatarURL)
-                    message.channel.send({embed})
-                }  
-            }).catch(function (err) {
-                message.channel.send("Sorry, that user doesn't seem to exist, double check your spelling and try again!") // catching error
-            });
-        } else {
-            message.channel.send("Please provide a valid username, e.g. '-search ROBLOX'.") 
-        }
-    }
+ 
+function isCommand(command, message){
+    var command = command.toLowerCase();
+    var content = message.content.toLowerCase();
+    return content.startsWith(prefix + command);
+}
+ 
+client.on('message', (message) => {
+    if (message.author.bot) return; // Dont answer yourself.
+    var args = message.content.split(/[ ]+/)
+   
+    if(isCommand('setrank', message)){
+       if(!message.member.roles.some(r=>["RANKHERE"].includes(r.name)) ) // OPTIONAL - Checks if the sender has the specified roles to carry on further
+        return message.reply("You can't use this command.");
+        var username = args[1]
+        var rankIdentifier = Number(args[2]) ? Number(args[2]) : args[2];
+        if (!rankIdentifier) return message.channel.send("Please enter a rank");
+        if (username){
+            message.channel.send(`Checking ROBLOX for ${username}`)
+            roblox.getIdFromUsername(username)
+            .then(function(id){
+                roblox.getRankInGroup(groupId, id)
+                .then(function(rank){
+                    if(maximumRank <= rank){
+                        message.channel.send(`${id} is rank ${rank} and not promotable.`)
+                    } else {
+                        message.channel.send(`${id} is rank ${rank} and promotable.`)
+                        roblox.setRank(groupId, id, rankIdentifier)
+                        .then(function(newRole){
+                            message.channel.send(`Changed rank to ${newRole.name}`)
+                        }).catch(function(err){
+                            console.error(err)
+                            message.channel.send("Failed to change rank.")
+                        });
+                    }
+                }).catch(function(err){
+                    message.channel.send("Couldn't get that player in the group.")
+                });
+            }).catch(function(err){
+                message.channel.send(`Sorry, but ${username} doesn't exist on ROBLOX.`)
+          });
+      } else {
+          message.channel.send("Please enter a username.")
+      }
+      return;
+  }
 })
